7710bce575a3116281ff2fd11008a75365f6567d
Durga Kumar <95281727+hackdurga@users.noreply.github.com>
2023-08-16 23:02:48 +0530

    Update display_gameover
    changes the size of the board
    changes the font size

--- Tic-Tac-Toe Game/main.py
+++ Tic-Tac-Toe Game/main.py
@@ -1,13 +1,9 @@
-# Author: aqeelanwar
-# Created: 12 March,2020, 7:06 PM
-# Email: aqeel.anwar@gatech.edu
-
 from tkinter import *
 import numpy as np
 
-size_of_board = 600
+size_of_board = 750
 symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
-symbol_thickness = 50
+symbol_thickness = 30
 symbol_X_color = '#EE4035'
 symbol_O_color = '#0492CF'
 Green_color = '#7BC043'
